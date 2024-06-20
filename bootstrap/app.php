fbba1e05442dd094926438e4d76612c8bd540e60
dedi.irawan <dedi.irawan@kemlu.go.id>
2024-06-20 14:45:37 +0700

    add unmask to bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Set umask to ensure files are created with proper permissions
+umask(0002);
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,4 +18,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
