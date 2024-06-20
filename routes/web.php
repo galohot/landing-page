0ffa17b98a712735c6c5e2653dfe16c30c65bb68
dedi.irawan <dedi.irawan@kemlu.go.id>
2024-06-20 19:21:04 +0700

    prefix route wiht landing page

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,21 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SectionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/landing-page',[SectionController::class,'index'])->name('section.index');
-Route::post('/landing-page/contact', [ContactFormController::class, 'store'])->name('contact.store');
+Route::prefix('landing-page')->group(function () {
+    Route::get('/',[SectionController::class,'index'])->name('section.index');
+    Route::post('/contact', [ContactFormController::class, 'store'])->name('contact.store');
+});
 
 
-Route::get('/landing-page/dashboard', function () {
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/landing-page/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/landing-page/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/landing-page/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
