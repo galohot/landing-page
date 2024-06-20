300074c205229bbcda782607d931408ef317968c
dedi.irawan <dedi.irawan@kemlu.go.id>
2024-06-20 11:36:19 +0700

    edit route for setails

--- resources/views/guess-layout/details.blade.php
+++ resources/views/guess-layout/details.blade.php
@@ -8,13 +8,8 @@
                     <div class="text-center col-lg-3 col-md-6">
                         <div class="mt-5">
                             <div class="mb-2">
-                                @foreach ($section->thumbnails as $thumbnail)
-                                    @if ($thumbnail->is_active)
-                                        <img src="{{ asset("../storage/{$thumbnail->thumbnail_path}") }}"
-                                            alt="Sturdy Themes" class="rounded-circle"
-                                            style="width: 3rem; height: 3rem; object-fit: contain;">
-                                    @endif
-                                @endforeach
+                                <img src="{{ asset("../storage/{$section->main_image}") }}" alt="Sturdy Themes"
+                                    class="rounded-circle" style="width: 3rem; height: 3rem; object-fit: contain;">
                             </div>
                             <h3 class="mb-2 h4">{{ $section->description }}</h3>
                             <p class="mb-0 text-muted">{{ $section->content }}</p>
