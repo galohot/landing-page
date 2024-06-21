69a820535a47e423fb0ef68de8869cf516823ca5
dedi.irawan <dedi.irawan@kemlu.go.id>
2024-06-21 09:25:45 +0700

    site title

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>IAF | MSP</title>
+        <title>IAF | MSP | 2024</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="{{ asset('../build/assets/assets/kemlu-favicon.png') }}" />
         <!-- Bootstrap Icons-->
